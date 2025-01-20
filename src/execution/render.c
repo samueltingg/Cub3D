edfd464565d2fffadbdeb54e34adc4d899b28c02
Samuel Ting <sting@u82z03s01.42kl.edu.my>
2025-01-20 16:46:20 +0800

    rearrange .cub files to maps/

--- src/execution/render.c
+++ src/execution/render.c
@@ -6,19 +6,19 @@
 /*   By: sting <sting@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 13:43:32 by sting             #+#    #+#             */
-/*   Updated: 2025/01/20 16:11:59 by sting            ###   ########.fr       */
+/*   Updated: 2025/01/20 16:28:22 by sting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	render_background(t_img *img, int color)
+void	render_half_background(t_img *img, int color)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	while (i < WINDOW_HEIGHT)
+	while (i < WINDOW_HEIGHT/2)
 	{
 		j = 0;
 		while (j < WINDOW_WIDTH)
@@ -35,6 +35,7 @@ int	render(void *param)
 	if (data->win_ptr == NULL)
 		return (1);
 	ft_bzero(data->img.addr, WINDOW_HEIGHT * WINDOW_WIDTH * (data->img.bits_per_pixel / 8));
+	render_half_background(&data->img, data->tex.ceiling_color);
 	raycasting(data);
 	render_map(data, data->map);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.img_ptr, 0,
